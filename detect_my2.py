962b38745e2011fa182067dd0b16ae640e068eec
furqonmhmmd <100257714+furqonmhmmd@users.noreply.github.com>
2023-06-10 11:02:55 +0700

    Update detect_my2.py

--- detect_my2.py
+++ detect_my2.py
@@ -53,7 +53,7 @@ from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreensh
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, smart_inference_mode, time_synchronized
+from utils.torch_utils import select_device, smart_inference_mode, time_sync
 
 
 @smart_inference_mode()
